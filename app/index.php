906df90e916df4bbfc49b3c989f993d018ddac9a
doko972 david <doko972@gmail.com>
2024-07-30 14:34:26 +0200

    commit

--- app/index.php
+++ app/index.php
@@ -43,17 +43,21 @@ include 'includes/_database.php';
     </header>
     <main>
         <h1 class="main-ttl">trailshare - <br> la communauté des amateurs de randonnées et de cyclotourisme</h1>
-
         <section class="trail-create">
             <h2 class="trail-create-ttl">Créez votre parcours</h2>
             <img class="trail-create-img" src="./img//Rectangle 61.png" alt="cycliste">
-            <p class="trail-create-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi modi magni reprehenderit quibusdam quod deserunt laborum natus cumque harum nisi, quia aut architecto qui dicta amet repellendus numquam obcaecati in odio. Illum, blanditiis animi magni harum tempore explicabo alias ducimus aspernatur, enim corrupti dolorum doloribus atque delectus! Fugit, aliquid tenetur?</p>
+            <p class="trail-create-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi modi magni
+                reprehenderit quibusdam quod deserunt laborum natus cumque harum nisi, quia aut architecto qui dicta
+                amet repellendus numquam obcaecati in odio. Illum, blanditiis animi magni harum tempore explicabo alias
+                ducimus aspernatur, enim corrupti dolorum doloribus atque delectus! Fugit, aliquid tenetur?</p>
             <button class="trail-create-btn btn">Créer un parcours</button>
         </section>
         <section class="trail-card">
             <img class="trail-card-img" src="./img//route_4-1.webp" alt="clecy">
             <p class="trail-card-txt">CLE Suisse Normande<br> 107 km - Difficile</p>
-            <p class="trail-card-txt">Découvrez ce parcours de vélo de 108,7 km à proximité de Cormelles-le-Royal. Ce parcours emprunte 82 km de routes et 26,7 km de pistes cyclables. Il présente une ascension cumulée de plus de 1160m</p>
+            <p class="trail-card-txt">Découvrez ce parcours de vélo de 108,7 km à proximité de Cormelles-le-Royal. Ce
+                parcours emprunte 82 km de routes et 26,7 km de pistes cyclables. Il présente une ascension cumulée de
+                plus de 1160m</p>
             <button class="btn">Voir la fiche parcours</button>
         </section>
     </main>
