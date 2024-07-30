bfc7254a5994fc1e7eaee9eb6b4f5232bff26232
doko972 david <doko972@gmail.com>
2024-07-30 11:16:53 +0200

    up date main branch with ayman_ branch

--- app/actions.php
+++ app/actions.php
@@ -4,7 +4,7 @@ include 'includes/_database.php';
 include 'includes/_functions.php';
 include 'includes/_config.php';
 // var_dump(getAllroutes($dbCo));
-var_dump(getRouteDetails($dbCo,1));
+// var_dump(getRouteDetails($dbCo,1));
 
 // if (!isset($_REQUEST['action'])) {
 //     redirectTo('index.php');
