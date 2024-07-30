623ce397d7cc9ff3606aeab7ec180d131806eeb4
khallaf <engayman.khalaf@gmail.com>
2024-07-30 08:59:30 +0200

    pull from main

--- app/includes/_functions.php
+++ app/includes/_functions.php
@@ -2,31 +2,36 @@
 include '_database.php';
 include '_config.php';
 
-function generateToken() {
+function generateToken()
+{
     if (!isset($_SESSION['token']) || !isset($_SESSION['tokenExpire']) || $_SESSION['tokenExpire'] < time()) {
         $_SESSION['token'] = md5(uniqid(mt_rand(), true));
         $_SESSION['tokenExpire'] = time() + 60 * 15;
     }
 }
 
-function isRefererOk(): bool {
+function isRefererOk(): bool
+{
     global $globalUrl;
     return isset($_SERVER['HTTP_REFERER']) && str_contains($_SERVER['HTTP_REFERER'], $globalUrl);
 }
 
-function isTokenOk(?array $data = null): bool {
+function isTokenOk(?array $data = null): bool
+{
     if (!is_array($data)) $data = $_REQUEST;
     return isset($_SESSION['token']) && isset($data['token']) && $_SESSION['token'] === $data['token'];
 }
 
-function addError(string $errorMsg): void {
+function addError(string $errorMsg): void
+{
     if (!isset($_SESSION['errorsList'])) {
         $_SESSION['errorsList'] = [];
     }
     $_SESSION['errorsList'][] = $errorMsg;
 }
 
-function redirectTo(string $url): void {
+function redirectTo(string $url): void
+{
     if (headers_sent()) {
         echo "<script>location.href='$url';</script>";
     } else {
@@ -34,7 +39,8 @@ function redirectTo(string $url): void {
     }
     exit;
 }
-function preventCSRF(string $redirectUrl = 'index.php'): void {
+function preventCSRF(string $redirectUrl = 'index.php'): void
+{
     if (!isRefererOk()) {
         addError('referer');
         redirectTo($redirectUrl);
@@ -45,7 +51,8 @@ function preventCSRF(string $redirectUrl = 'index.php'): void {
     }
 }
 
-function validateToken($token) {
+function validateToken($token)
+{
     return $token === $_SESSION['token'];
 }
 
@@ -72,11 +79,11 @@ function getAllroutes(PDO $dbCo): void
  * @param PDO $dbCo database connection
  * @return void
  */
-function getArouteDetails(PDO $dbCo):void
+function getArouteDetails(PDO $dbCo, int $idRoute): void
 {
     $query = $dbCo->prepare("SELECT * FROM route WHERE id_route =:idRoute;");
     $isQueryOk = $query->execute();
-    $routeDetails = $query->fetchAll();
+    $routeDetails = $query->fetchAll(['idRoute' => $idRoute]);
 
     if (!$isQueryOk) {
     }
