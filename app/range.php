94d2c6424287d456eba12201eadb795fca1068f7
doko972 david <doko972@gmail.com>
2024-07-31 16:09:19 +0200

    branch ayman_ done

--- app/range.php
+++ app/range.php
@@ -7,7 +7,8 @@
             <div class="range-options">
                 <div class="range-choice">
                     <label for="distance">Distance:</label>
-                    <input name="distance" type="range" id="distance" min="1" max="100" oninput="this.nextElementSibling.value = this.value">
+                    <input name="distance" type="range" id="distance" min="1" max="100"
+                        oninput="this.nextElementSibling.value = this.value">
                     <output>100</output> km
                 </div>
 
@@ -30,7 +31,7 @@
                 <div class="range-choice">
                     <legend>Mode:</legend>
                     <?php
-                    $classRoutes  = getClassRoutes($dbCo);
+                    $classRoutes = getClassRoutes($dbCo);
                     foreach ($classRoutes as $classRoute) {
                         echo AddsHtmlClassRoute($classRoute);
                     }
@@ -44,34 +45,5 @@
             </div>
         </form>
     </div>
-
-        <div class="range-choice">
-            <legend>Difficulté:</legend>
-            <div class="range_radio-alg">
-                <label for="easy">Facile</label><br>
-                <input type="radio" id="easy" name="level" value="easy">
-            </div>
-            <div class="range_radio-alg">
-                <label for="medium">Moyen</label><br>
-                <input type="radio" id="medium" name="level" value="medium">
-            </div>
-            <div class="range_radio-alg">
-                <label for="hard">Difficile</label><br>
-                <input type="radio" id="hard" name="level" value="hard">
-            </div>
-        </div>
-
-        <div class="range-choice">
-            <legend>Mode:</legend>
-            <div class="range_radio-alg">
-                <label for="onfoot">A Pieds</label><br>
-                <input type="radio" id="onfoot" name="class_route" value="onfoot">
-            </div>
-            <div class="range_radio-alg">
-                <label for="bike">Vélo</label><br>
-                <input type="radio" id="bike" name="class_route" value="bike">
-            </div>
-        </div>
-        <button class="range-btn btn" type="submit">Rechercher</button>
     </div>
 </section>
