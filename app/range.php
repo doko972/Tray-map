2c186ae95b52fb49553577e396e47e224c467064
doko972 david <doko972@gmail.com>
2024-08-01 09:08:13 +0200

    first commit today

--- app/range.php
+++ app/range.php
@@ -20,15 +20,11 @@
                         echo AddsHtmlDifficulty($difficulty);
                     }
                     ?>
-                    <!-- <input type="radio" id="easy" name="difficulty_name" value="1">
-                    <label for="easy">Facile</label><br>
-                    <input type="radio" id="medium" name="difficulty_name" value="2">
-                    <label for="medium">Moyen</label><br>
-                    <input type="radio" id="hard" name="difficulty_name" value="3">
-                    <label for="hard">Difficile</label><br> -->
                 </div>
+            </div>
 
-                <div class="range-choice">
+            <div class="range-choice">
+                <div class="range_radio-alg">
                     <legend>Mode:</legend>
                     <?php
                     $classRoutes = getClassRoutes($dbCo);
@@ -36,14 +32,39 @@
                         echo AddsHtmlClassRoute($classRoute);
                     }
                     ?>
-                    <!-- <input type="radio" id="onfoot" name="class_route" value="1">
-                    <label for="onfoot">A Pieds</label><br>
-                    <input type="radio" id="bike" name="class_route" value="2">
-                    <label for="bike">Vélo</label><br> -->
                 </div>
                 <button class="range-btn btn" type="submit">Rechercher</button>
             </div>
         </form>
     </div>
+
+    <!-- <div class="range-choice">
+            <legend>Difficulté:</legend>
+            <div class="range_radio-alg">
+                <label for="easy">Facile</label><br>
+                <input type="radio" id="easy" name="level" value="easy">
+            </div>
+            <div class="range_radio-alg">
+                <label for="medium">Moyen</label><br>
+                <input type="radio" id="medium" name="level" value="medium">
+            </div>
+            <div class="range_radio-alg">
+                <label for="hard">Difficile</label><br>
+                <input type="radio" id="hard" name="level" value="hard">
+            </div>
+        </div>
+
+        <div class="range-choice">
+            <legend>Mode:</legend>
+            <div class="range_radio-alg">
+                <label for="onfoot">A Pieds</label><br>
+                <input type="radio" id="onfoot" name="class_route" value="onfoot">
+            </div>
+            <div class="range_radio-alg">
+                <label for="bike">Vélo</label><br>
+                <input type="radio" id="bike" name="class_route" value="bike">
+            </div>
+        </div> -->
+    <!-- <button class="range-btn btn" type="submit">Rechercher</button> -->
     </div>
 </section>
