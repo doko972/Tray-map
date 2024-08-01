0c625d6354200f356de8a7d180b9388d045cd0e4
doko972 david <doko972@gmail.com>
2024-08-01 16:51:53 +0200

    correct range.php

--- app/search-route.php
+++ app/search-route.php
@@ -5,15 +5,44 @@ include 'includes/_functions.php';
 include 'includes/_config.php';
 include 'includes/_templates.php';
 
-$dataStrip = stripTagsArray($_REQUEST);
+if (!$_REQUEST['action'] === 'search') {
+
+    addError("referer");
+    redirectTo();
+}
+
+// preventCSRF();
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trail-Map</title>
+    <link rel="stylesheet" href="./file.scss//main.scss">
+</head>
+
+<body>
+<?php include 'header.php'; ?>
+<main>
+<?php
 
 if ($_REQUEST['action'] === 'search') {
+
+    $dataStrip = stripTagsArray($_REQUEST);
     $sqlResuest = constructSqlSearchRoute($dataStrip);
     $idRoutes = getRoutesBySearchParam($dbCo, $sqlResuest);
     foreach ($idRoutes as $value) {
         $route = getRouteDetails($dbCo, $value['id_route']);
         echo getHtmlProduct($route[0]);
     }
-
-    exit;
 }
+?>
+</main>
+</body>
+
