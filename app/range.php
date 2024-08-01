feb6e80990530f60310941cdc494319cb11f166f
khallaf <engayman.khalaf@gmail.com>
2024-08-01 14:57:27 +0200

    before pull doko

--- app/range.php
+++ app/range.php
@@ -1,49 +1,48 @@
-<section>
-    <div class="range-container">
-        <form class="range-search-dy" action="/action_page.php">
-            <input class="search-form" type="text" placeholder="Rechercher votre ville..." name="search">
-            <button type="submit" class="search-btn">
-                <img src="img/loupe.png" alt="Recherche" class="search-icon">
-            </button>
-        </form>
-        <div class="range-options">
-            <div class="range-choice">
-                <div class="range_radio-ad">
-                    <label for="distance">Distance</label>
+<section class="range-container">
+    <div class="range-nav">
+        <form method="post" action="search-route.php">
+            <!-- //token  dont forget to send token-->
+            <input type="hidden" name="action" value="search">
+            <input class="range-search" type="text" placeholder="Search.." name="title">
+            <div class="range-options">
+                <div class="range-choice">
+                    <label for="distance">Distance:</label>
+                    <input name="distance" type="range" id="distance" min="1" max="100" oninput="this.nextElementSibling.value = this.value">
+                    <output>100</output> km
                 </div>
-                <div class="range-slider">
-                    <span>0</span>km
-                    <input class="range-input" type="range" id="distance" min="0" max="100"
-                        oninput="this.nextElementSibling.value = this.value">
-                    <output>100</output>+km
-                </div>
-            </div>
-            <div class="range-choice">
-                <div class="range-choice_md-ld">
-                    <legend>Difficulté</legend>
-                </div>
-                <?php
-                $difficulties = getDifficulties($dbCo);
-                foreach ($difficulties as $difficulty) {
-                    echo AddsHtmlDifficulty($difficulty);
-                }
-                ?>
-            </div>
-            <div class="range-choice_md">
-                <div class="range-choice_md-lg">
-                    <legend>Mode</legend>
-                </div>
-                <div class="range-choice_rd">
+
+                <div class="range-choice">
+                    <legend>Difficulté:</legend>
                     <?php
-                    $classRoutes = getClassRoutes($dbCo);
+                    $difficulties = getDifficulties($dbCo);
+                    foreach ($difficulties as $difficulty) {
+                        echo AddsHtmlDifficulty($difficulty);
+                    }
+                    ?>
+                    <!-- <input type="radio" id="easy" name="difficulty_name" value="1">
+                    <label for="easy">Facile</label><br>
+                    <input type="radio" id="medium" name="difficulty_name" value="2">
+                    <label for="medium">Moyen</label><br>
+                    <input type="radio" id="hard" name="difficulty_name" value="3">
+                    <label for="hard">Difficile</label><br> -->
+                </div>
+
+                <div class="range-choice">
+                    <legend>Mode:</legend>
+                    <?php
+                    $classRoutes  = getClassRoutes($dbCo);
                     foreach ($classRoutes as $classRoute) {
                         echo AddsHtmlClassRoute($classRoute);
                     }
                     ?>
+                    <!-- <input type="radio" id="onfoot" name="class_route" value="1">
+                    <label for="onfoot">A Pieds</label><br>
+                    <input type="radio" id="bike" name="class_route" value="2">
+                    <label for="bike">Vélo</label><br> -->
                 </div>
+                <button class="range-btn btn" type="submit">Rechercher</button>
             </div>
-        </div>
-        <button class="range-btn btn" type="submit">Rechercher</button>
+        </form>
     </div>
-    </div>
+
 </section>
