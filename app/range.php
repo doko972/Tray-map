2d691aa3ca7b7a9f5ed0f13dffd138d104c838ea
khallaf <engayman.khalaf@gmail.com>
2024-08-01 15:49:54 +0200

    add token

--- app/range.php
+++ app/range.php
@@ -1,52 +1,59 @@
+<?php
+
+echo getHtmlMessages($messages);
+
+echo getHtmlErrors($errors);
+
+?>
 <section>
     <div class="range-container">
-        <form class="range-search-dy" action="search-route.php" method="POST">
-            <input class="search-form" type="text" placeholder="Rechercher votre ville..." name="search">
-            <input type="hidden" name="action" value="search">
-            <!-- //token  dont forget to send token-->
-            <input type="hidden" name="action" value="search">
-            <button type="submit" class="search-btn">
-                <img src="img/loupe.png" alt="Recherche" class="search-icon">
-            </button>
-        </form>
-        <div class="range-options">
-            <div class="range-choice">
-                <div class="range_radio-ad">
-                    <label for="distance">Distance</label>
+        <form action="search-route.php" method="POST">
+            <form class="range-search-dy" action="search-route.php" method="POST">
+                <input type="hidden" id="token" name="token" value="<?= $_SESSION['token'] ?>">
+                <input class="search-form" type="text" placeholder="Rechercher votre ville..." name="title">
+                <input type="hidden" name="action" value="search">
+                <input type="hidden" name="action" value="search">
+                <button type="submit" class="search-btn">
+                    <img src="img/loupe.png" alt="Recherche" class="search-icon">
+                </button>
+            </form>
+            <div class="range-options">
+                <div class="range-choice">
+                    <div class="range_radio-ad">
+                        <label for="distance">Distance</label>
+                    </div>
+                    <div class="range-slider">
+                        <span>0</span>km
+                        <input class="range-input" type="range" id="distance" min="0" max="100" oninput="this.nextElementSibling.value = this.value">
+                        <output>100</output>+km
+                    </div>
                 </div>
-                <div class="range-slider">
-                    <span>0</span>km
-                    <input class="range-input" type="range" id="distance" min="0" max="100"
-                        oninput="this.nextElementSibling.value = this.value">
-                    <output>100</output>+km
-                </div>
-            </div>
-            <div class="range-choice">
-                <div class="range-choice_md-ld">
-                    <legend>Difficulté</legend>
-                </div>
-                <?php
-                $difficulties = getDifficulties($dbCo);
-                foreach ($difficulties as $difficulty) {
-                    echo AddsHtmlDifficulty($difficulty);
-                }
-                ?>
-            </div>
-            <div class="range-choice_md">
-                <div class="range-choice_md-lg">
-                    <legend>Mode</legend>
-                </div>
-                <div class="range-choice_rd">
+                <div class="range-choice">
+                    <div class="range-choice_md-ld">
+                        <legend>Difficulté</legend>
+                    </div>
                     <?php
-                    $classRoutes  = getClassRoutes($dbCo);
-                    foreach ($classRoutes as $classRoute) {
-                        echo AddsHtmlClassRoute($classRoute);
+                    $difficulties = getDifficulties($dbCo);
+                    foreach ($difficulties as $difficulty) {
+                        echo AddsHtmlDifficulty($difficulty);
                     }
                     ?>
                 </div>
-                <button class="range-btn btn" type="submit">Rechercher</button>
+                <div class="range-choice_md">
+                    <div class="range-choice_md-lg">
+                        <legend>Mode</legend>
+                    </div>
+                    <div class="range-choice_rd">
+                        <?php
+                        $classRoutes = getClassRoutes($dbCo);
+                        foreach ($classRoutes as $classRoute) {
+                            echo AddsHtmlClassRoute($classRoute);
+                        }
+                        ?>
+                    </div>
+                </div>
             </div>
-        </div>
-        <button class="range-btn btn" type="submit">Rechercher</button>
+            <button class="range-btn btn" type="submit">Rechercher</button>
     </div>
+    </form>
 </section>
